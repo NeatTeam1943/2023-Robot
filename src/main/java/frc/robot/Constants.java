25ef4b9c98313e9be5c0dcea4eaa814df299a928
LiranBratt2121 <brattlirannin@gmail.com>
2023-01-08 02:48:31 +0200

    src: Constants: Remove unnecessary class inside the Constants class

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -13,7 +13,4 @@ package frc.robot;
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
-  public static class OperatorConstants {
-    public static final int kDriverControllerPort = 0;
-  }
 }
