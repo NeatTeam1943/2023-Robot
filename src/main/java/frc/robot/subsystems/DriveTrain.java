637d602067d0350de4faa0e3f60a0a5eab32ce6a
LiranBratt2121 <brattlirannin@gmail.com>
2023-01-09 18:39:48 +0200

    src: DriveTrian: made support for encoder

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -4,14 +4,8 @@
 
 package frc.robot.subsystems;
 
-import javax.xml.crypto.KeySelector.Purpose;
-
-import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
-import com.fasterxml.jackson.databind.introspect.ConcreteBeanPropertyBase;
-
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.motorcontrol.MotorController;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
@@ -22,9 +16,7 @@ public class DriveTrain extends SubsystemBase {
   private WPI_TalonFX backRightMotor;
   private WPI_TalonFX frontLeftMotor; 
   private WPI_TalonFX frontRightMotor;
-  
   private DifferentialDrive differentialDrive;
-
 
   public DriveTrain() {
     this.backLeftMotor = new WPI_TalonFX(Constants.kBackFrontMotor);
@@ -38,11 +30,14 @@ public class DriveTrain extends SubsystemBase {
     this.differentialDrive = new DifferentialDrive(this.frontLeftMotor, this.frontRightMotor);
   }
 
+  public WPI_TalonFX getBackLeftMotor(){return this.backLeftMotor;}
+  
   public void robotArcadeDrive(double move, double rot){
     if (move < 0.05) {move = 0;}
     if (rot < 0.05) {rot = 0;}
     differentialDrive.arcadeDrive(move, rot);
   }
+
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
