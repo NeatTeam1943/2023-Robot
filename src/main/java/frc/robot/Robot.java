e929a669fa8eabb3d07c505a337a74bf2b898360
LiranBratt2121 <brattlirannin@gmail.com>
2023-01-10 18:20:26 +0200

    src: Robot: Add robot values for the simulation

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,6 +4,7 @@ the WPILib BSD license file in the root directory of this project.*/
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
