ddafbc59ee42d4ac8773da3364c2597ecebc850b
LiranBratt2121 <brattlirannin@gmail.com>
2023-01-11 02:57:27 +0200

    src: Robot: Fix unused alert

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -17,7 +17,7 @@ import edu.wpi.first.wpilibj2.command.CommandScheduler;
  */
 public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
-
+  @SuppressWarnings("unused")
   private RobotContainer robotContainer;
 
   /**
