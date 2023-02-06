25978207b1e1284c94f9d308a1ac0d26970b849a
LiranBratt2121 <brattlirannin@gmail.com>
2023-02-06 18:39:46 +0200

    src: constants: Add simulation subclass

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -15,4 +15,13 @@ public final class Constants {
     public static final int kRightFrontPort = 3;
     public static final int kRightRearPort = 4;
   }
+  
+  public static class DriveTrainSimulation{
+    public static final double kUpdateTime = 0.02;
+    public static final int kCountsPerRev = 4096;
+    public static final double kWheelRadiusInches = 3;
+    public static final double kSensorGearRatio = 1.0;
+    public static final double kGearRatio = 10.71; 
+    public static final int k100msPerSecond = 10;
+  }
 }
