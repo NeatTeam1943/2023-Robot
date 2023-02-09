a3948404affce1e43e38f90f2e3bef238e22b968
LiranBratt2121 <brattlirannin@gmail.com>
2023-02-09 18:49:41 +0200

    src: Removed redundant autonomus autonomous function

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -48,14 +48,7 @@ public class Robot extends TimedRobot {
   
   /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
   @Override
-  public void autonomousInit() {
-    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
-
-    // schedule the autonomous command (example)
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.schedule();
-    }
-  }
+  public void autonomousInit() {}
 
   /** This function is called periodically during autonomous. */
   @Override
