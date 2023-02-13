ced1e4c0f823ae2499757e60f0f4bf9698ffd313
LiranBratt2121 <brattlirannin@gmail.com>
2023-02-13 18:04:46 +0200

    src: driveArcade: Add overload to create a difference between simDrive and actual driving

--- src/main/java/frc/robot/commands/DriveArcade.java
+++ src/main/java/frc/robot/commands/DriveArcade.java
@@ -1,18 +1,28 @@
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.subsystems.DriveTrain;
 import frc.robot.subsystems.SimDrive;
 
 public class DriveArcade extends CommandBase {
-  private SimDrive m_driveTrain;
-  private XboxController m_joystick;
+  private DriveTrain m_driveTrain;
+  private SimDrive m_driveTrainSim;
+  private CommandXboxController m_joystick;
+  private boolean isSim;
 
-  public DriveArcade(SimDrive m_simDrive, XboxController joystick) {
-    m_driveTrain = m_simDrive;
+  public DriveArcade(DriveTrain driveTrain, CommandXboxController joystick) {
+    m_driveTrain = driveTrain;
     m_joystick = joystick;
-
+    this.isSim = false; 
     addRequirements(m_driveTrain);
+  }
+
+  public DriveArcade(SimDrive driveTrain, CommandXboxController joystick) {
+    m_driveTrainSim = driveTrain;
+    m_joystick = joystick;
+    this.isSim = true; 
+    addRequirements(m_driveTrainSim);
   }
 
   // Called when the command is initially scheduled.
@@ -23,9 +33,17 @@ public class DriveArcade extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    double mov = m_joystick.getLeftX();
-    double rot = m_joystick.getLeftY();
-    m_driveTrain.robotArcadeDrive(-mov, -rot);
+    if (this.isSim){
+      double mov = m_joystick.getLeftX();
+      double rot = m_joystick.getLeftY();
+      m_driveTrainSim.robotArcadeDrive(-mov, -rot);
+    }
+    else{
+      double mov = m_joystick.getLeftX();
+      double rot = m_joystick.getLeftY();
+      m_driveTrain.arcadeDrive(-mov, -rot);
+    }
+
   }
 
   // Called once the command ends or is interrupted.
