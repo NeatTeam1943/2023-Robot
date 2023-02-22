3b932a0c85a1a8678f6e7e99b93b433f4a68fb5d
NeatTeam Software <47425793+neatteamsoftware@users.noreply.github.com>
2023-02-22 17:46:53 +0200

    src: commands: turnpid: Add squareInputs as true

--- src/main/java/frc/robot/commands/TurnPID.java
+++ src/main/java/frc/robot/commands/TurnPID.java
@@ -27,7 +27,7 @@ public class TurnPID extends PIDCommand {
         () -> drivetrain.getHeading() + angleGoal,
         // This uses the output
         output -> {
-          drivetrain.arcadeDrive(0, output + m_feedforward.calculate(drivetrain.getHeading() + angleGoal));
+          drivetrain.arcadeDrive(0, output + m_feedforward.calculate(drivetrain.getHeading() + angleGoal), true);
         });
     // Use addRequirements() here to declare subsystem dependencies.
     getController().enableContinuousInput(-180, 180);
