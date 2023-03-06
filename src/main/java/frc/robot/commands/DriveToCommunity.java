392154baaf956db46ff42788d24fc495db702bc8
NeatTeam Software <47425793+neatteamsoftware@users.noreply.github.com>
2023-03-06 20:27:57 +0200

    src: drive_to_community: Find original code YOHUUUUU

--- src/main/java/frc/robot/commands/DriveToCommunity.java
+++ src/main/java/frc/robot/commands/DriveToCommunity.java
@@ -9,17 +9,26 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.DriveTrain;
 
 public class DriveToCommunity extends CommandBase {
+
   private DriveTrain m_drive;
   private ADIS16448_IMU m_imu;
 
-  private boolean m_passChargeStation;
-  private boolean m_hasSafeDistance;
+  private boolean m_onChargingStation = false;
+  private boolean m_passChargingStation = false;
+  private boolean m_hasSafeDistance = false;
+  
+  // Arabic line
+  private boolean m_passedStaition = false; 
+
+  private boolean m_inverted;
+
   private double m_setpoint;
 
-  /** Creates a new DriveToCommunity. */
-  public DriveToCommunity(DriveTrain drive) {
-    m_drive = drive;
-    m_imu = drive.getIMU();
+  /** Creates a new GyroAuto. */
+  public DriveToCommunity(DriveTrain driveTrain, boolean inverted) {
+    m_drive = driveTrain;
+    m_imu = m_drive.getIMU();
+    m_inverted = inverted;
 
     addRequirements(m_drive);
   }
@@ -27,33 +36,59 @@ public class DriveToCommunity extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    m_drive.calibrateIMU();
-
-    m_passChargeStation = false;
+    m_imu.calibrate();
+    m_onChargingStation = false;
+    m_passChargingStation = false;
     m_hasSafeDistance = false;
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    final double m_voltage = 0.15;
-    final double m_threshold = 1; // Just should be higher than zero
-    final double m_angleX = m_imu.getGyroAngleX(); // Create a cleaner boolean value to check for stabilizion 
-
-    boolean m_isStable = (-1 < m_angleX && m_angleX < 1); 
-
-    m_drive.arcadeDrive(-m_voltage, 0, false);
-
-    if (m_angleX > m_threshold) { // Should not check for something that can't happen
-      m_passChargeStation = true;
+    final double voltage = 0.15;
+    if (m_inverted){
+      if (m_imu.getGyroAngleX() < -10) {
+        m_onChargingStation = true;
+      }
+  
+      if (m_imu.getGyroAngleX() > -1 && m_imu.getGyroAngleX() < 1 && m_onChargingStation) {
+        m_passChargingStation = true; 
+      }
+  
+      if (m_onChargingStation && m_passChargingStation && !m_passedStaition){
+        m_setpoint = m_drive.getDistance() - 0.25; 
+        m_passedStaition = true; 
+      }
+  
+      if (m_drive.getDistance() < m_setpoint && m_passedStaition){
+        m_hasSafeDistance = true; 
+      }
+  
+  
+    } else{
+      if (m_imu.getGyroAngleX() > 10) {
+        m_onChargingStation = true;
+      }
+  
+      if (m_imu.getGyroAngleX() > -1 && m_imu.getGyroAngleX() < 1 && m_onChargingStation) {
+        m_passChargingStation = true; 
+      }
+  
+      if (m_onChargingStation && m_passChargingStation && !m_passedStaition){
+        m_setpoint = m_drive.getDistance() + 0.25; 
+        m_passedStaition = true; 
+      }
+  
+      if (m_drive.getDistance() > m_setpoint && m_passedStaition){
+        m_hasSafeDistance = true; 
+      }
     }
 
-    if (m_passChargeStation && m_isStable && !m_hasSafeDistance) { // Fix naming
-      m_hasSafeDistance = true;
-      m_setpoint = m_drive.getDistance() + 0.25; // Fix formatting
+    if (m_inverted){
+      m_drive.arcadeDrive(voltage, 0, false);
+    } else{
+      m_drive.arcadeDrive(-voltage, 0, false);
     }
-
-    System.out.println("Driving to community, AngleX: " + m_angleX + ", Did pass: " + m_passChargeStation);
   }
 
   // Called once the command ends or is interrupted.
@@ -65,7 +100,6 @@ public class DriveToCommunity extends CommandBase {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    // return m_drive.getDistance() > m_setpoint;
-    return m_hasSafeDistance; 
+    return m_onChargingStation && m_passChargingStation && m_hasSafeDistance;
   }
 }
