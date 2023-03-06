52ab394474c0ecd6285f3793ae88000504d2f98d
NeatTeam Software <47425793+neatteamsoftware@users.noreply.github.com>
2023-03-06 20:19:13 +0200

    src: drive_to_community: Fix logic

--- src/main/java/frc/robot/commands/DriveToCommunity.java
+++ src/main/java/frc/robot/commands/DriveToCommunity.java
@@ -12,8 +12,8 @@ public class DriveToCommunity extends CommandBase {
   private DriveTrain m_drive;
   private ADIS16448_IMU m_imu;
 
-  private boolean m_didPassChargeStation;
-  private boolean m_startMoving;
+  private boolean m_passChargeStation;
+  private boolean m_hasSafeDistance;
   private double m_setpoint;
 
   /** Creates a new DriveToCommunity. */
@@ -28,29 +28,32 @@ public class DriveToCommunity extends CommandBase {
   @Override
   public void initialize() {
     m_drive.calibrateIMU();
-    m_didPassChargeStation = false;
-    m_startMoving = false;
+
+    m_passChargeStation = false;
+    m_hasSafeDistance = false;
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    final double voltage = 0.15;
-    final double threshold = 3;
-    final double angleX = m_imu.getGyroAngleX();
+    final double m_voltage = 0.15;
+    final double m_threshold = 1; // Just should be higher than zero
+    final double m_angleX = m_imu.getGyroAngleX(); // Create a cleaner boolean value to check for stabilizion 
 
-    m_drive.arcadeDrive(-voltage, 0, false);
+    boolean m_isStable = (-1 < m_angleX && m_angleX < 1); 
 
-    if (!m_didPassChargeStation && angleX > threshold) {
-      m_didPassChargeStation = true;
+    m_drive.arcadeDrive(-m_voltage, 0, false);
+
+    if (m_angleX > m_threshold) { // Should not check for something that can't happen
+      m_passChargeStation = true;
     }
 
-    if (m_didPassChargeStation && (-1 < angleX && angleX < 1) && !m_startMoving){
-      m_startMoving = true;
-      m_setpoint = m_drive.getDistance()+0.98;
+    if (m_passChargeStation && m_isStable && !m_hasSafeDistance) { // Fix naming
+      m_hasSafeDistance = true;
+      m_setpoint = m_drive.getDistance() + 0.25; // Fix formatting
     }
 
-    System.out.println("Driving to community, AngleX: " + angleX + ", Did pass: " + m_didPassChargeStation);
+    System.out.println("Driving to community, AngleX: " + m_angleX + ", Did pass: " + m_passChargeStation);
   }
 
   // Called once the command ends or is interrupted.
@@ -62,6 +65,7 @@ public class DriveToCommunity extends CommandBase {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return m_drive.getDistance() > m_setpoint;
+    // return m_drive.getDistance() > m_setpoint;
+    return m_hasSafeDistance; 
   }
 }
