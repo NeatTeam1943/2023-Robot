b1ed18fa877c35ba9f51f0ce23613afa5fbf0ad0
NeatTeam Software <47425793+neatteamsoftware@users.noreply.github.com>
2023-03-07 17:49:44 +0200

    wip

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -112,6 +112,6 @@ public class RobotContainer {
     MoveDoor close = new MoveDoor(m_door, -0.7);
     ParallelCommandGroup moveToCommunity = new ParallelCommandGroup(close, driveToCommunity);
     
-    return Commands.sequence(open, moveToCommunity, ); 
+    return Commands.sequence(open, moveToCommunity); 
   }
 }
